4e92cf194f3932d7285f37e887ec6133f3e05b04
Puttirat <puttirat.lima@bumail.net>
2025-03-11 01:00:45 +0700

    finish

--- Assets/Scripts/Gravity.cs
+++ Assets/Scripts/Gravity.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Gravity : MonoBehaviour
@@ -6,18 +7,30 @@ public class Gravity : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     Rigidbody rb;
     private const float G = 0.00674f;
+
+    public static List<Gravity> gravityObjectsList ;
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
+
+        if (gravityObjectsList == null)
+        {
+            gravityObjectsList = new List<Gravity>();
+        }
+        
+        gravityObjectsList.Add(this);
     }
     private void FixedUpdate()
     {
-        
+        foreach (Gravity obj in gravityObjectsList)
+        {
+            Attract(obj);
+        }
     }
 
     void Attract(Gravity other) 
     {
-       Rigidbody rbOther = GetComponent<Rigidbody>();
+       Rigidbody rbOther = other.rb ;
 
        Vector3 direction = rb.position - rbOther.position;
        
@@ -27,5 +40,6 @@ public class Gravity : MonoBehaviour
        
        float forceMagnitude = G * ((rb.mass * rbOther.mass) / Mathf.Pow(distance, 2));
        Vector3 force = forceMagnitude * direction.normalized;
+       rbOther.AddForce(force);
     }    
 }
