15c0cce365e4890cc87fe16c5dd19915231bd971
Puttirat <puttirat.lima@bumail.net>
2025-03-11 00:37:20 +0700

    Gravity script add

--- Assets/Scripts/Gravity.cs
+++ Assets/Scripts/Gravity.cs
@@ -1,16 +1,31 @@
+using System;
 using UnityEngine;
 
 public class Gravity : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    Rigidbody rb;
+    private const float G = 0.00674f;
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    private void FixedUpdate()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    void Attract(Gravity other) 
     {
-        
-    }
+       Rigidbody rbOther = GetComponent<Rigidbody>();
+
+       Vector3 direction = rb.position - rbOther.position;
+       
+       float distance = direction.magnitude;
+
+       if (distance == 0) {return;}
+       
+       float forceMagnitude = G * ((rb.mass * rbOther.mass) / Mathf.Pow(distance, 2));
+       Vector3 force = forceMagnitude * direction.normalized;
+    }    
 }
